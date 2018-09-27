ae8113bfc5593d1813f95600545f771d9adc4c8f
ClementLemay <lclem444@aol.com>
2018-09-27 09:59:08 +0200

    Version Stable Ulric/Alexandre/Clément

--- Modeles/Conteneurs/conteneurGenre.php
+++ Modeles/Conteneurs/conteneurGenre.php
@@ -41,19 +41,16 @@ Class conteneurGenre
 		}
 		for($j=0;$j<$i;$j++)
 		{
-			echo 'valeur de j avant'.$j;
 			$liste = $liste.'<tr>';
 			$a = 0;
 			 while(($a<3) && ($j<$i))
 			 {
-				 echo 'toto';
 				 $unGenre=$tab[$j];
 				 $j++;
 				 $liste = $liste.'<td class="text-white td-table">'.$unGenre->getIdGenre().'</td><td class="text-white td-table"><p class="flotte"><img src=Images/'.$j.'.jpg></p><p>'.$unGenre->getLibelleGenre().'</p></td>';
 				 $a++;
 			 }
 			 $j--;
-			 echo 'valeur de j apres'.$j;
 		$liste = $liste.'</tr>';
 		}
 			$liste=$liste."</tbody></table></div></div>";
